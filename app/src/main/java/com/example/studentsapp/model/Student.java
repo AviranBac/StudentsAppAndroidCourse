2bafeec04254e33ea39bc25bf154271fc3adf376
Dor Haliva <haliva84@gmail.com>
2022-12-09 15:52:23 +0200

    feat(newStudentActivity): add new student activity design

--- app/src/main/java/com/example/studentsapp/model/Student.java
+++ app/src/main/java/com/example/studentsapp/model/Student.java
@@ -3,12 +3,16 @@ package com.example.studentsapp.model;
 public class Student {
     private String name;
     private String id;
+    private String phone;
+    private String address;
     private String avatarUrl;
     private Boolean checked;
 
-    public Student(String name, String id, String avatarUrl, Boolean checked) {
+    public Student(String name, String id, String phone, String address, String avatarUrl, Boolean checked) {
         this.name = name;
         this.id = id;
+        this.phone = phone;
+        this.address = address;
         this.avatarUrl = avatarUrl;
         this.checked = checked;
     }
@@ -27,6 +31,22 @@ public class Student {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getAvatarUrl() {
